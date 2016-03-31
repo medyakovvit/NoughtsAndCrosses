7193762f3747136476cba11c2a2505f5ab5182bd
medyakovvit <medyakovvit@gmail.com>
2016-04-01 01:59:51 +0600

    Expose BoardModel to qml via context property

--- Gui/main.cpp
+++ Gui/main.cpp
@@ -1,11 +1,18 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QtQml>
+
+#include <boardmodel.h>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    BoardModel model;
+    engine.rootContext()->setContextProperty("boardModel", &model);
+
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
     return app.exec();
