4e7daf2a97d8598e8428c5a5cbc3006085d411f9
medyakovvit <medyakovvit@gmail.com>
2016-04-02 15:43:34 +0600

    Delete not used code

--- Gui/main.cpp
+++ Gui/main.cpp
@@ -12,13 +12,7 @@ int main(int argc, char *argv[])
 
     QQmlApplicationEngine engine;
 
-    BoardModel model;
     Game game;
-
-    //qmlRegisterType<Player>("my.X_And_O.Player", 1, 0, "Player");
-    qmlRegisterUncreatableType<Player>("my.X_And_O.Player", 1, 0, "Player",
-                                       QString("Player is uncreatable in qml"));
-    engine.rootContext()->setContextProperty("boardModel", &model);
     engine.rootContext()->setContextProperty("myGame", &game);
 
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
