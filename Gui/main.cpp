384b829f51a1bcf4fea831965d997ee4df4e1076
medyakovvit <medyakovvit@gmail.com>
2016-04-13 00:06:58 +0500

    Register classes Player andBoardModel to Qml
    as uncreatable types.

--- Gui/main.cpp
+++ Gui/main.cpp
@@ -2,15 +2,17 @@
 #include <QQmlApplicationEngine>
 #include <QtQml>
 
-#include <boardmodel.h>
 #include <game.h>
 #include <player.h>
+#include <boardmodel.h>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    qmlRegisterUncreatableType<Player>("my.Xs_and_Os.Game", 1, 0, "Player", "Player is uncreatable type");
+    qmlRegisterUncreatableType<BoardModel>("my.Xs_and_Os.Game", 1, 0, "BoardModel", "BoardModel is uncreatable type");
 
     Game game;
     engine.rootContext()->setContextProperty("myGame", &game);
